e9abadda14e285d84498da9858fc34ef21065e86
neharani6 <neharani6796@gmail.com>
2021-01-27 10:43:30 +0530

    Initial commit

---
+++ src/app/sidenav-overview-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/** @title Basic sidenav */
+@Component({
+  selector: 'sidenav-overview-example',
+  templateUrl: 'sidenav-overview-example.html',
+  styleUrls: ['sidenav-overview-example.css'],
+})
+export class SidenavOverviewExample {
+  opened = false;
+}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
